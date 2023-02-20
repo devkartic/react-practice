7de69d0124633452baee721cde304a0cb8db6901
devkartic <dev.kartic@gmail.com>
2023-02-20 21:50:20 +0600

    Working with todo completing

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -17,17 +17,35 @@ class TodoList extends Component {
         this.onchangeHandler = this.onchangeHandler.bind(this)
         this.onsubmitHandler = this.onsubmitHandler.bind(this)
         this.taskEditHandler = this.taskEditHandler.bind(this)
+        this.taskDeletedHandler = this.taskDeletedHandler.bind(this)
     }
 
     onchangeHandler(event) {
+        let id = this.state.todo.id;
+        if (this.state.todo.id==='') {
+            id = Math.max.apply(null, this.state.todos.map(function (element) {
+                return element.id;
+            }));
+            id += 1;
+        }
+
         this.setState({
-            todo: {id: 33, name: event.target.value}
+            todo: {id, name: event.target.value}
         })
     }
 
     onsubmitHandler(event) {
         event.preventDefault();
         if (this.state.edit) {
+            let id = this.state.todo.id;
+            let name = this.state.todo.name;
+            this.setState({
+                todos: this.state.todos.map((element) => element.id === this.state.todo.id ? {
+                    ...element,
+                    id,
+                    name
+                } : element)
+            });
             this.setState({edit: false});
         } else {
             if (this.state.todo.name !== '') {
@@ -46,8 +64,13 @@ class TodoList extends Component {
         }
     }
 
-    render() {
+    taskDeletedHandler(todo_id) {
+        this.setState({
+            todos: this.state.todos.filter((element) => element.id !== todo_id)
+        });
+    }
 
+    render() {
         const taskList = this.state.todos.map((todo, index) => {
             return (
                 <tr key={index}>
@@ -57,7 +80,7 @@ class TodoList extends Component {
                     <td>
                         <Button color="warning" className="me-1"
                                 onClick={() => this.taskEditHandler(todo.id)}>Edit</Button>
-                        <Button color="danger">Delete</Button>
+                        <Button color="danger" onClick={() => this.taskDeletedHandler(todo.id)}>Delete</Button>
                     </td>
                 </tr>
             );
